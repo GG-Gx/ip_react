593592c36b268327f0689adab34775b5538096cf
GG-Gx <78389994+GG-Gx@users.noreply.github.com>
2024-11-11 16:58:34 -0300

    workin well

--- IP_REACT/src/main.jsx
+++ IP_REACT/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+
+        <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
